94cd82529088e6af7f46fc7482c8f84e7034be02
Feldwor <BoQsc@users.noreply.github.com>
2021-11-17 14:57:23 +0200

    Add files via upload

--- command_prompt_handler.cmd
+++ command_prompt_handler.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
+DEL "%appdata%\dmd_handlers.txt"
 FOR %%i IN (%SystemDrive%\D\dmd2\windows\bin\*.exe) DO (
 	SET "outputFile=%%~ni.cmd"
 	ECHO %SystemRoot%\!outputFile!
