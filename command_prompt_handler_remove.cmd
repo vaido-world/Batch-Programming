cc13955c9666cc92863c6bf1fb2b48c2c4711005
Feldwor <BoQsc@users.noreply.github.com>
2021-11-17 15:18:34 +0200

    Update command_prompt_handler_remove.cmd

--- command_prompt_handler_remove.cmd
+++ command_prompt_handler_remove.cmd
@@ -5,5 +5,6 @@ FOR /F %%u IN (%appdata%\dmd_handlers.txt) DO DEL %%u
 DEL "%appdata%\dmd_handlers.txt"
 
 REM Deletes itself from startup folder. Requires administrator prompt on Windows startup..
+REM Does not do anything for now.
 IF "%~DP0" == "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\" DEL %0
 pause
