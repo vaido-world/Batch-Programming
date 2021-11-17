230dbc0c2184ea2d9ecc0d05e3e05fe38d904a55
Feldwor <BoQsc@users.noreply.github.com>
2021-11-17 15:17:48 +0200

    Update command_prompt_handler_remove.cmd

--- command_prompt_handler_remove.cmd
+++ command_prompt_handler_remove.cmd
@@ -4,5 +4,6 @@ REM Requires Administrator Privilegies
 FOR /F %%u IN (%appdata%\dmd_handlers.txt) DO DEL %%u
 DEL "%appdata%\dmd_handlers.txt"
 
+REM Deletes itself from startup folder.
 IF "%~DP0" == "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\" DEL %0
 pause
