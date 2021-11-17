5368a69eb85d1dc71de4e23a774ae3da0c146870
Feldwor <BoQsc@users.noreply.github.com>
2021-11-17 15:27:35 +0200

    Update command_prompt_handler_install.cmd

--- command_prompt_handler_install.cmd
+++ command_prompt_handler_install.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
-REM command_prompt_handler.cmd Gives  Immediate Access to the binaries on already opened Command Prompt Windows.
-REM command_prompt_handler_remove.cmd Removes handler files on the next Windows Startup.
+REM command_prompt_handler.cmd         Gives Immediate Access to the binaries on already opened Command Prompt Windows.
+REM command_prompt_handler_remove.cmd  Removes handler files on the next Windows Startup.
 
 cd "%~dp0"
 REM Requires Administrator Privilegies
